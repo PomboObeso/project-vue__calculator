25bb843d862d35b3f03d850d8107996a33a2f44f
PomboObeso <matheus.gomes201@gmail.com>
2025-11-11 17:24:10 -0300

    Project Vue - Visual Configs template

--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,27 @@ export default {
 </script>
 
 <style>
+@font-face {
+    font-family: "RobotoMono" ;
+    src: url("./fonts/RobotoMono-Thin.ttf");
+}
+* {
+    font-family: "RobotoMono", monospace;
+}
 
+body {
+    margin: 0;
+    padding: 0;
+}
+
+#app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+
+    color: #fff;
+    background: linear-gradient(to right, rgb(82,102, 115), rgb(39, 44, 70));
+}
 </style>
