6d4756eb42d22c4b4b3439f280a555a9232e6b99
PomboObeso <matheus.gomes201@gmail.com>
2025-11-13 12:15:04 -0300

    Adding Display properties and Styles itself

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,13 +1,23 @@
 <template>
-    <div class="display">0</div>
+    <div class="display">{{value}}</div>
 </template>
 
 <script>
 export default {
-
+    props: ['value']
 }
 </script>
 
 <style>
-
+    .display {
+        grid-column: span 4;
+        background-color: #0009;
+        
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        padding: 15px;
+        font-size: 2.2em;
+        overflow: hidden;
+    }
 </style>
